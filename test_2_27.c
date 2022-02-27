3e718d440dadc244d733e62a434b60f135182e6d
pgwstr <2712278005@qq.com>
2022-02-27 18:39:29 +0800

    打印9*9乘法口诀

---
+++ test_2_27.c
@@ -0,0 +1,18 @@
+//
+// Created by 潘光伟 on 2022/2/27.
+//
+//打印9*9乘法口诀
+#include "stdio.h"
+int main()
+{
+    int i=0;
+    int j=0;
+    for(i=1;i<=9;i++){
+        for (j = 1; j <=i ; j++) {
+            printf("%dx%d=%d ",i,j,i*j);
+        }
+        printf("\n");//打印完一行记得换行
+    }
+
+    return 0;
+}

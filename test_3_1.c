d58263ad9240c7337960934d1cc8ca7a15f13e8d
pgwstr <2712278005@qq.com>
2022-03-01 17:19:53 +0800

    用递归逆序一个数组，不能使用string函数

---
+++ test_3_1.c
@@ -0,0 +1,17 @@
+//
+// Created by 潘光伟 on 2022/3/1.
+//
+
+//用递归逆序一个数组，不能使用string函数
+#include "stdio.h"
+remove_string(cahr* str)
+{
+
+}
+int main()
+{
+    char arr[]="abcdef";
+    remove_string(arr);
+    printf("%s\n",arr);
+    return 0;
+}

0796b69d52c0dcf4e2a51d09c2620c2e7c4f861e
pgwstr <2712278005@qq.com>
2022-03-04 21:32:12 +0800

    冒泡排序一个正序数组

---
+++ test_3_4_2.c
@@ -0,0 +1,26 @@
+//
+// Created by 潘光伟 on 2022/3/4.
+//
+//冒泡排序一个正序数组
+#include "stdio.h"
+int blbl_sort(int arr[],int size){
+    int i=0;
+    int j=0;
+    for(int i = 0; i < size-1;++i) {
+        for (int j = 0; j < size-1-i; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                int tep = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tep;
+            }
+        }
+    }
+    printf("%d\n",arr);
+}
+int main()
+{
+    int arr[10]={9,8,7,6,5,4,3,2,1,0};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    blbl_sort(arr,size);
+    return 0;
+}

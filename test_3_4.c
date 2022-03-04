5d000daf2c3b9ac82fd3e67ae7ad84917b30ba85
pgwstr <2712278005@qq.com>
2022-03-04 20:32:52 +0800

    数组练习

--- test_3_4.c
+++ test_3_4.c
@@ -18,19 +18,30 @@
 //    return 0;
 //}
 //打印数组存储位置
+//int main()
+//{
+//    int arr[3][4]={{1,2},{3,4},{5,6}};
+//    int i=0;
+//    int j=0;
+//    for (int i = 0; i < 3; ++i) {
+//        for (int j = 0; j <4 ; ++j) {
+//            printf("arr[%d][%d]=%p\n ",i,j,arr[i][j]);
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//
+//}
+//用指针打印出数组中的存储
 int main()
 {
     int arr[3][4]={{1,2},{3,4},{5,6}};
+     int  *p=arr;
     int i=0;
-    int j=0;
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j <4 ; ++j) {
-            printf("arr[%d][%d]=%p\n ",i,j,arr[i][j]);
-        }
-//        printf("\n");
+    for (int i = 0; i < 12; ++i) {
+        printf("%d ",*p);
+        p++;
     }
     return 0;
-
 }
-
 

7d52286d655b8a701df0f2a50591a778d75762e4
pgwstr <2712278005@qq.com>
2022-03-04 20:29:11 +0800

    数组练习

--- test_3_4.c
+++ test_3_4.c
@@ -3,17 +3,34 @@
 //
 //数组练习
 #include "stdio.h"
+//int main()
+//{
+//   int arr[3][4]={{1,2},{3,4},{5,6}};
+//   int i=0;
+//   int j=0;
+//    for (int i = 0; i <3; ++i) {
+//        for (int j=0;j<4;++j){
+//            printf("%d ",arr[i][j]);
+//        }
+//        printf("\n");
+//    }
+//
+//    return 0;
+//}
+//打印数组存储位置
 int main()
 {
-   int arr[3][4]={{1,2},{3,4},{5,6}};
-   int i=0;
-   int j=0;
-    for (int i = 0; i <3; ++i) {
-        for (int j=0;j<4;++j){
-            printf("%d ",arr[i][j]);
+    int arr[3][4]={{1,2},{3,4},{5,6}};
+    int i=0;
+    int j=0;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j <4 ; ++j) {
+            printf("arr[%d][%d]=%p\n ",i,j,arr[i][j]);
         }
-        printf("\n");
+//        printf("\n");
     }
-
     return 0;
+
 }
+
+

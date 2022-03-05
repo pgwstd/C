b6b27f0323785828b9b7b96c9cc2a25e1686ee0f
pgwstr <2712278005@qq.com>
2022-03-05 23:47:42 +0800

    三子棋

--- game_3_5.c
+++ game_3_5.c
@@ -7,8 +7,10 @@ void InintBoard(char board[ROW][COL],int row,int col)
 {
     int i=0;
     int j=0;
-    for (i=0;i<row;i++){
-        for (j=0;j<col;j++){
+    for (i=0;i<row;i++)
+    {
+        for (j=0;j<col;j++)
+        {
             board[i][j]=' ';
         }
     }
@@ -17,9 +19,11 @@ void InintBoard(char board[ROW][COL],int row,int col)
 void DisplayBoard(char board[ROW][COL],int row,int col)
 {
     int i=0;
-    for (i = 0;  i< row; i++) {
+    for (i = 0;  i< row; i++)
+    {
         printf(" %c | %c | %c \n", board[i][0], board[i][1], board[i][2]);
-        if (i < row - 1) {
+        if (i < row - 1)
+        {
             printf("---|---|---\n");
         }
     }

2809c84b24f3e9b586ec40833cc2b728eb5e5c1b
pgwstr <2712278005@qq.com>
2022-03-06 23:50:20 +0800

    三子棋

--- game_3_5.c
+++ game_3_5.c
@@ -75,63 +75,57 @@ void PlayerMove(char board[ROW][COL],int row,int col)
 //电脑下棋
 void ComputerMove(char board[ROW][COL],int row,int col) {
     printf("电脑走:>\n");
-    while (1)
-    {
+    while (1) {
         int x = rand() % row;
         int y = rand() % col;
         if (board[x][y] == ' ') {
             //电脑下子
-            board[x - 1][y - 1] = '#';
+            board[x][y] = '#';
             break;
         }
     }
-int IsFull(char board[ROW][COL],int row,int col)
-{
-     int i=0;
-     int j=0;
-    for (int i = 0; i < row; ++i) {
-        for (int j = 0; j < col; ++j) {
-            if(board[i][j]==' ')
-                return 0;
+    int IsFull(char board[ROW][COL], int row, int col)
+    {
+        int i = 0;
+        int j = 0;
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (board[i][j] == ' ') {
+                    return 0;
+                }
+            }
         }
+        return 1;
     }
-}
-    return 1;
-}
 //游戏输赢判断
-char IsWin(char board[ROW][COL],int row,int col)
-{
-    int i=0;
-    //判断三行
-    for (i = 0; i < row; ++i) {
-        if(board[i][0] == board[i][1] && board[i][1]==board[i][2] && board[i][1]!=' ')
-        {
-            return board[i][1];
+    char IsWin(char board[ROW][COL], int row, int col)
+    {
+        int i = 0;
+        //判断三行
+        for (i = 0; i < row; ++i) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][1] != ' ') {
+                return board[i][1];
+            }
         }
-    }
-    //判断三列
-    for (i = 0; i < col; ++i) {
-        if(board[0][i] == board[1][i] && board[1][i]==board[2][i] && board[1][i]!=' ')
-        {
-            return board[1][i];
+        //判断三列
+        for (i = 0; i < col; ++i) {
+            if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[1][i] != ' ') {
+                return board[1][i];
+            }
         }
-    }
-    //判断对角
-        if(board[0][0] == board[1][1] && board[1][1]==board[2][2] && board[1][1]!=' ')
-        {
+        //判断对角
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != ' ') {
             return board[1][1];
         }
-    if(board[0][2] == board[1][1] && board[1][1]==board[2][0] && board[1][1]!=' ')
-    {
-        return board[1][1];
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] != ' ') {
+            return board[1][1];
+        }
+        //平局
+        //如果棋盘满了就返回Q，没满返回0
+        int ret = IsFull(board, row, col);
+        if (ret == 1) {
+            return 'Q';
+        }
+        //不满就继续
+        return 'C';
     }
-    //平局
-    //如果棋盘满了就返回Q，没满返回0
-    int ret=IsFull(board,row,col);
-    if(ret==1)
-    {
-        return 'Q';
-    }
-    //不满就继续
-    return 'C';
-}

f9c0af06403adf8365da318dcbcfe138a9a7802f
pgwstr <2712278005@qq.com>
2022-03-09 22:48:17 +0800

    数据结构

--- test_3_9.c
+++ test_3_9.c
@@ -4,6 +4,8 @@
 //数据结构作业
 #include "stdio.h"
 #include "stdlib.h"
+#include "iostream"
+using namespace std;
 #define OK 1
 #define ERROR 0
 #define OVERFLOW -2
@@ -19,24 +21,25 @@ typedef struct
 
 Status InitList(SqList &L)
 {
-    L.elem=new ElemType[MAXSIZE];
-    if(!L.elem)
-        exit(OVERFLOW);
-    L.length=0;
+  L.elem=new ElemType[MAXSIZE];
+     if(!L.elem)
+       exit(OVERFLOW);
+   L.length=0;
     return OK;
 }
 
 Status GetElem(SqList L,int i,ElemType &e)
 {
     if(i<1||i>L.length)
-        return ERROR;
+       return ERROR;
     e=L.elem[i-1];
-        return OK;
+       return OK;
 }
 
 int LocateElem(SqList L,ElemType e)
 {
-    for (int i = 0; i < L.length; ++i) {
+    for (int i = 0; i < L.length; ++i)
+    {
         if(L.elem[i]==e)
             return i+1;
         return 0;
@@ -45,39 +48,39 @@ int LocateElem(SqList L,ElemType e)
 
 Status ListInsert(SqList &L,int i,ElemType e)
 {
-    if((i<1)||(i>L.length+1))
+     if((i<1)||(i>L.length+1))
         return ERROR;
-    if(L.length==MAXSIZE)
-        return ERROR;
+      if(L.length==MAXSIZE)
+         return ERROR;
     for(int j=L.length-1;j>=i-1;j--)
-        L.elem[j+1]=L.elem[j];
-    L.elem[i-1]=e;
-    ++L.length;
-    return OK;
+      L.elem[j+1]=L.elem[j];
+     L.elem[i-1]=e;
+      ++L.length;
+       return OK;
 }
 Status ListDelete(SqList &L,int i)
 {
-    if((i<1)||(i>L.length))
+     if((i<1)||(i>L.length))
         return ERROR;
-    for(int j=i;j<=L.length-1j;j++)
-    L.elem[j-1]=L.elem[j];
-    --L.length;
-    return OK;
+     for(int j=i;j<=L.length;j++)
+       L.elem[j-1]=L.elem[j];
+           --L.length;
+        return OK;
 }
 int main()
 {
     int i=0;
     int x=0;
     int y=0;
-    sqList myList;
+    SqList myList;
     //初始化顺序表
     InitList(myList);
     //建立顺序表
-    for (int i = 1; i < 10; ++i)
+    for (i = 1; i < 10; ++i)
     {
         ListInsert(myList,i,i);
         printf("建立后的顺序是:");
-        for (int i = 1; i <=myList.length ; ++i)
+        for (i = 1; i <=myList.length ; i++)
         {
             GetElem(myList,i,x);
             printf("%d   ",x);
@@ -98,13 +101,12 @@ int main()
         //删除
         ListDelete(myList,4);
         printf("删除第4个数后的顺序表是:");
-        for (int i = 1; i <=myList.length ; ++i)
+        for (i = 1; i <=myList.length ; i++)
         {
             GetElem(myList,i,x);
             printf("%d   ",x);
         }
+        printf("\n");
     }
-
-
 }
 

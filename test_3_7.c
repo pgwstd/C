e521d6768019e502b8903f0adcc7ce75ef05080e
pgwstr <2712278005@qq.com>
2022-03-09 22:27:01 +0800

    扫雷

--- test_3_7.c
+++ test_3_7.c
@@ -11,25 +11,48 @@ void menu()
     printf("**************************\n");
 }
 
-int main()
-{
-    int input=0;
-    do
+int main() {
+    void game()
     {
-        menu();
-        printf("请选择:>");
-        scanf("%d",&input);
-        switch (input) {
-            case 1:
-                printf("开始游戏");
-                break;
-            case 0:
-                printf("游戏结束!\n");
-                break;
-            default:
-                printf("输入错误，请重新输入！\n");
-        }
+        char mine[ROWS][COLS] = {0};//存放布置好的雷的信息
+        char show[ROWS][COLS] = {0};//存放排查出的雷的信息
+        //初始化棋盘
+        InitBoard(mine, ROWS, COLS, '0');//'0'
+        InitBoard(show, ROWS, COLS, '*');//'*'
 
-    } while (input);
+        //打印一下棋盘
+        DisplayBoard(show, ROW, COL);
+
+        //布置雷
+        SetMine(mine, ROW, COL);
+        //DisplayBoard(mine, ROW, COL);
+
+        //排查雷
+        FindMine(mine, show, ROW, COL);
+    }
+
+    int main()
+    {
+        int input = 0;
+        srand((unsigned int) time(NULL));
+
+        do {
+            menu();
+            printf("请选择:>");
+            scanf("%d", &input);
+            switch (input) {
+                case 1:
+                    game();//扫雷游戏
+                    break;
+                case 0:
+                    printf("退出游戏\n");
+                    break;
+                default:
+                    printf("选择错误，重新选择\n");
+                    break;
+            }
+        } while (input);
+
         return 0;
+    }
 }

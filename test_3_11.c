a46bcb87959b2321915f48f2e53724324935e51a
pgwstr <2712278005@qq.com>
2022-03-12 13:17:26 +0800

    3_12

--- test_3_11.c
+++ test_3_11.c
@@ -1,9 +1,19 @@
 //
 // Created by 潘光伟 on 2022/3/11.
 //
+//交换两个数的变量，不能使用第三个变量，即int a=3,int b=5;交换后a=5,b=3;
 #include "stdio.h"
-int mian()
+int main()
 {
+    int a=3;
+    int b=5;
+    printf("a=%d ",a);
+    printf("b=%d\n",b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("a=%d ",a);
+    printf("b=%d\n",b);
     return 0;
 }
 

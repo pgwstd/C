9907aa393b907501d1a3e9c60995e9f37af2eebe
pgwstr <2712278005@qq.com>
2022-03-12 23:23:00 +0800

    3_12

--- test_3_12_2.c
+++ test_3_12_2.c
@@ -2,6 +2,14 @@
 // Created by 潘光伟 on 2022/3/12.
 //
 #include "stdio.h"
+void test1(int a[])
+{
+    printf("%d\n",sizeof(a));//指针大小和类型没关系，和它的系统寻址能力有关
+}
+void test2(char b[])
+{
+    printf("%d\n",sizeof(b));//一般在32位系统中的指针大小一般为4个，64位系统为8个字节
+}
 int main()
 {
 //    int a=2;
@@ -14,13 +22,15 @@ int main()
 //    printf("%d\n",s);
     int arr[10]={0};
     char arr2[10]={0};
-    printf("%d\n",sizeof(arr));
-    printf("%d\n",sizeof(arr2));
-    printf("%d\n",sizeof(char));
-    printf("%d\n",sizeof(short));
-    printf("%d\n",sizeof(int));
-    printf("%d\n",sizeof(float));
-    printf("%d\n",sizeof(long));
-    printf("%d\n",sizeof(double));
+//    printf("%d\n",sizeof(arr));
+//    printf("%d\n",sizeof(arr2));
+//    printf("%d\n",sizeof(char));
+//    printf("%d\n",sizeof(short));
+//    printf("%d\n",sizeof(int));
+//    printf("%d\n",sizeof(float));
+//    printf("%d\n",sizeof(long));
+//    printf("%d\n",sizeof(double));
+    test1(arr);
+    test2(arr2);
     return 0;
 }

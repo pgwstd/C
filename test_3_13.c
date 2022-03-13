4cb9a245f26cd1a99f8f2c762591319fcddeb003
pgwstr <2712278005@qq.com>
2022-03-13 12:29:18 +0800

    3_13

---
+++ test_3_13.c
@@ -0,0 +1,18 @@
+//
+// Created by 潘光伟 on 2022/3/13.
+//
+#include "stdio.h"
+int main()
+{
+    int a=3;
+    int b=5;
+    int c=0;
+    if(a&b)
+    {
+        printf("与运算！\n");
+    }
+    if(a|c){
+        printf("或运算！\n");
+    }
+    return 0;
+}

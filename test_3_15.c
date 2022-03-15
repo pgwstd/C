7c88bdbcadfeeef3304d8397f4b22fe009435af3
pgwstr <2712278005@qq.com>
2022-03-15 17:12:50 +0800

    3_15错误的代码分析

--- test_3_15.c
+++ test_3_15.c
@@ -21,7 +21,7 @@
 int main()
 {
     int i=0;
-    int count=(++i)+(++i)+(++i);//有问题的代码，++i不懂是从哪一个开始赋值计算
+    int count=(++i)+(++i)+(++i);//有问题的代码，++i不懂是从哪一个开始计算,顺序是未知的
     printf("%d\n",count);
     return 0;
 }

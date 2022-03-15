6d144883c81dce1d1722b5ff81375b4e6fe65769
pgwstr <2712278005@qq.com>
2022-03-15 16:43:37 +0800

    3_15错误的代码分析

--- test_3_15.c
+++ test_3_15.c
@@ -1,11 +1,20 @@
 //
 // Created by 潘光伟 on 2022/3/15.
 //
+//有问题的代码分析
 #include "stdio.h"
+int fun()
+{
+    int b=1;
+    ++b;
+}
+
 int main()
 {
 
-
+    int  count;
+    count=fun()-fun() * fun();//这个代码是有错的，fun调用的赋值时候，不能确定是那个先运算或赋值
+    printf("%d\n",count);
     return 0;
 }
 

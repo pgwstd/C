47b6b268f2e9a67086fa65ed3e5504a31136ee7d
pgwstr <2712278005@qq.com>
2022-03-15 17:06:22 +0800

    3_15错误的代码分析

--- test_3_15.c
+++ test_3_15.c
@@ -2,18 +2,26 @@
 // Created by 潘光伟 on 2022/3/15.
 //
 //有问题的代码分析
+//#include "stdio.h"
+//int fun()
+//{
+//    static int b=1;
+//    return ++b;
+//}
+//
+//int main()
+//{
+//
+//    int  count;
+//    count=fun()-fun() * fun();//这个代码是有错的，fun调用的赋值时候，不能确定是那个先运算或赋值
+//    printf("%d\n",count);
+//    return 0;
+//}
 #include "stdio.h"
-int fun()
-{
-    static int b=1;
-    return ++b;
-}
-
 int main()
 {
-
-    int  count;
-    count=fun()-fun() * fun();//这个代码是有错的，fun调用的赋值时候，不能确定是那个先运算或赋值
+    int i=0;
+    int count=(++i)+(++i)+(++i);//有问题的代码
     printf("%d\n",count);
     return 0;
 }

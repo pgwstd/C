4f016556364151b363f1c43156a55c0264597a60
pgwstr <2712278005@qq.com>
2022-03-15 19:29:31 +0800

    3_15作业

--- test_3_15_3.c
+++ test_3_15_3.c
@@ -6,8 +6,14 @@
 //实现reverse函数，函数完成数组元素的逆置
 void reverse(int arr3[],int size)
 {
-    for (int i = size-1; i >=0 ; --i) {
-        printf("%d ",arr3[i]);
+    int left=0;
+    int right=size-1;
+    while (left<right) {
+        int tmp = arr3[left];
+        arr3[left] = arr3[right];
+        arr3[right] = tmp;
+        left++;
+        right--;
     }
 }
 //实现print函数，打印数组的每一个函数
@@ -16,13 +22,14 @@ void print(int arr2[],int size)
     for (int i = 0; i < size; ++i) {
         printf("%d ",arr2[i]);
     }
+    printf("\n");
 }
 //实现init函数使数组初始化为0
 void init(int arr1[],int size)
 {
     for (int i = 0; i < size; ++i) {
         arr1[i]=0;
-        printf("%d ", arr1[i]);
+
     }
 
 }
@@ -30,10 +37,12 @@ int main()
 {
     int arr[10]={1,2,3,4,5,6,7,8,9,10};
     int size=(sizeof(arr)/sizeof(arr[0]));
-    init(arr,size);
-    printf("\n");
     print(arr,size);
-    printf("\n");
+
     reverse(arr,size);
+    print(arr,size);
+
+    init(arr,size);
+    print(arr,size);
     return 0;
 }

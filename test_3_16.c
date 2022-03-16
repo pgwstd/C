75345788edb4bad4864b8dfd15941cc29a56b3d8
pgwstr <2712278005@qq.com>
2022-03-16 11:32:02 +0800

    3_15

---
+++ test_3_16.c
@@ -0,0 +1,19 @@
+//
+// Created by 潘光伟 on 2022/3/16.
+//
+//
+#include "stdio.h"
+void bubble_sort(int a[],int sz)
+{
+
+}
+int main()
+{
+    int number=0;
+    int arr[]={0};
+//    printf("请输入你要输入数的个数:\n");
+    scanf("%d",arr);
+    int size=(sizeof(arr)/sizeof(arr[0]));
+    bubble_sort(arr,size);
+    return 0;
+}

dc97658adcc86be394aa5810710d12aab1f02bf8
pgwstr <2712278005@qq.com>
2022-03-16 11:40:25 +0800

    3_15

--- test_3_16.c
+++ test_3_16.c
@@ -5,14 +5,21 @@
 #include "stdio.h"
 void bubble_sort(int a[],int sz)
 {
-
+//    int tmp=0;
+    for (int i = 0; i < sz; ++i) {
+//        tmp=a[i];
+//        a[i+1]=a[i];
+//        a[i]=tmp;
+     if(a[i]>a[i+1])
+         a[i]=a[i+1];
+    }
 }
 int main()
 {
-    int number=0;
-    int arr[]={0};
+//    int number=0;
+    int arr[]={0,1,2,3,4,5,6,7,8,9};
 //    printf("请输入你要输入数的个数:\n");
-    scanf("%d",arr);
+//    scanf("%d",arr);
     int size=(sizeof(arr)/sizeof(arr[0]));
     bubble_sort(arr,size);
     return 0;

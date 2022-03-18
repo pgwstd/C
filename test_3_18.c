9ba781f918b51bc267a79cfddf683b6cc9d772fa
pgwstr <2712278005@qq.com>
2022-03-18 23:29:46 +0800

    3_18

--- test_3_18.c
+++ test_3_18.c
@@ -2,14 +2,24 @@
 // Created by 潘光伟 on 2022/3/18.
 //
 #include "stdio.h"
+int my_strlen(char *str)
+{
+    char *strl=str;
+    while (*str!='\0'){
+        str++;
+    }
+    return str-strl;
+}
 int main()
 {
-    int arr[]={1,2,3,4,5,6,7,8,9,10};
-    int *p=arr;
-    int *pend=arr+9;
-    while (p<=pend){
-        printf("%d\n",*p);
-        p++;
-    }
+//    int arr[]={1,2,3,4,5,6,7,8,9,10};
+//    int *p=arr;
+//    int *pend=arr+9;
+//    while (p<=pend){
+//        printf("%d\n",*p);
+//        p++;
+//    }
+    int rel=my_strlen("abc");
+    printf("%d\n",rel);
     return 0;
 }

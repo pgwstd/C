a812dc7d4d5bcac1bb6cf0ed88e34d3b8c2f80a0
pgwstr <2712278005@qq.com>
2022-03-19 17:37:22 +0800

    作业

--- test_3_19_4.c
+++ test_3_19_4.c
@@ -1,25 +1,26 @@
 //
 // Created by 潘光伟 on 2022/3/19.
 //
+//输入五个数输出（运用指针和函数）
 #include <stdio.h>
 
 void InputArray(int *pa, int n)
 {
-    for (; *pa < *pa + n; pa++)
+    for (; *pa <  n; pa++)
         scanf("%d", pa);
 
 }
 void OutputArray(int *pa, int n)
 {
-    for(; *pa < *pa+n; pa++) {
-        printf("%4d", *pa);
+    for(; *pa <= n; pa++) {
+        printf("%d ", *pa);
     }
     printf("\n");
 }
 int main()
 {
     int a[5];
-    printf("Input five numbers:");
+    printf("请输入5个数:\n");
     InputArray(a, 5);
     OutputArray(a, 5);
 }

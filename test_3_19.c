a6311fa1b5ab2eb5995b3573c0667b92e0809a70
pgwstr <2712278005@qq.com>
2022-03-19 11:21:36 +0800

    输入一个自定义大小的数组，进行从小到大排序

--- test_3_19.c
+++ test_3_19.c
@@ -4,7 +4,8 @@
 #include "stdio.h"
 #include "stdlib.h"
 //输入一个自定义大小的数组，进行从小到大排序
-int main() {
+int main()
+{
     int n = 0;
     int *arr;
     printf("你要进行进行多少个数排序:\n");
@@ -14,11 +15,14 @@ int main() {
         printf("输入错误！\n");
     } else {
         printf("输入排序数字:\n");
-        for (int i = 0; i < n; ++i) {  //循环输入数组
+        for (int i = 0; i < n; ++i)
+        {  //循环输入数组
             scanf("%d", &arr[i]);
         }
-        for (int j = 0; j <n-1; j++) {  // 冒泡排序
-            if (arr[j] > arr[j + 1]) {
+        for (int j = 0; j <n-1; j++)
+        {  // 冒泡排序
+            if (arr[j] > arr[j + 1])
+            {
                 int tmp;
                 tmp = arr[j];
                 arr[j] = arr[j + 1];
@@ -27,7 +31,8 @@ int main() {
             }
         }
             printf("从小到大输入数组:\n");
-            for (int k = 0; k < n; k++) {  //循环输出
+            for (int k = 0; k < n; k++)
+            {  //循环输出
                 printf("%d ", arr[k]);
             }
 

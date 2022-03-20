615221b977c90f8f6ece197dc9f51d776dd5be18
pgwstr <2712278005@qq.com>
2022-03-20 18:49:44 +0800

    制做一个函数，使的从键盘输入数组的个数，要他们输出和

--- test_3_20.c
+++ test_3_20.c
@@ -4,14 +4,15 @@
 //制做一个函数，使的从键盘输入数组的个数，要他们输出和
 #include "stdio.h"
 
-void InputArray(int *p, int n) {
+int InputArray(int *p, int n) {    //输入函数
     printf("请输入你要输入的数:\n");
     for (int i = 0; i < n; ++i) {
         scanf("%d", &p[i]);
     }
+    return sizeof(*p)/sizeof(int*);
 }
 
-int OutputArray(int *p, int n) {
+int OutputArray(int *p, int n) {  //输出函数
     int sum = 0;
     for (int j = 0; j < n; ++j) {
         sum += p[j];
@@ -24,10 +25,13 @@ int main() {
     int n = 0;
     printf("请输入你要输入数组的个数\n");
     scanf("%d", &n);
-    InputArray(arr, n);
-    int ret = OutputArray(arr, n);
-    printf("输出他们和:\n");
-    printf("%d ", ret);
-    return 0;
+    int sz = InputArray(arr, n);
+    if (sz > n) {
+        printf("你输入的个数与实际规定的个数不一致！");
+    } else {
+        int ret = OutputArray(arr, n);
+        printf("输出他们和:\n");
+        printf("%d ", ret);
+        return 0;
+    }
 }
-

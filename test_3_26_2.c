b47ca0d4edff6a58825b9f731903617cce6738f9
pgwstr <2712278005@qq.com>
2022-03-26 22:48:38 +0800

    3_26

--- test_3_26_2.c
+++ test_3_26_2.c
@@ -1,6 +1,7 @@
 //
 // Created by 潘光伟 on 2022/3/26.
 //
+//求字符段大小
 #include "stdio.h"
 #include "assert.h"
 int my_strlen(const char *str)
@@ -9,6 +10,7 @@ int my_strlen(const char *str)
     assert(str!=NULL);
     while (*str!='\0') {
         count++;
+        str++;
     }
     return count;
 

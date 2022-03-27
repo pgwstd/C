715483226a8b1a759ba1cc612763f80eaa4d3078
pgwstr <2712278005@qq.com>
2022-03-27 21:19:40 +0800

    3_27

--- test_3_27_5.c
+++ test_3_27_5.c
@@ -10,7 +10,7 @@ int main()
     c2=98;
     char a1=0;
     char a2=0;
-    scanf("%c %c",&a1,&a2);
+    scanf("%c  %5c",&a1,&a2);
     printf("%c %c\n",a1,a2);
     printf("%c %c\n",c1,c2);
     printf("%d %d\n",c1,c2);

dcab843bd18bd56b64f0656f5ea8c946154c77fd
pgwstr <2712278005@qq.com>
2022-03-27 21:06:08 +0800

    3_27

---
+++ test_3_27_5.c
@@ -0,0 +1,15 @@
+//
+// Created by 潘光伟 on 2022/3/27.
+//
+//课后作业
+#include "stdio.h"
+int main()
+{
+    int c1,c2;
+    c1=97;
+    c2=98;
+    printf("%c %c\n",c1,c2);
+    printf("%d %d\n",c1,c2);
+    return 0;
+}
+

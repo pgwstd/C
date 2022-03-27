ff6ff7f59b631b277d5cf5df8d54c309630be679
pgwstr <2712278005@qq.com>
2022-03-27 20:32:33 +0800

    3_27

---
+++ test_3_27_2.c
@@ -0,0 +1,14 @@
+//
+// Created by 潘光伟 on 2022/3/27.
+//
+//课后作业
+#include "stdio.h"
+int main()
+{
+
+    printf("************\n");
+    printf(" very good! \n");
+    printf("************\n");
+    return 0;
+}
+

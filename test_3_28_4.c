da48a37272f56527be5361c5e340b18e638142b6
pgwstr <2712278005@qq.com>
2022-03-28 19:00:44 +0800

    3_28

--- test_3_28_4.c
+++ test_3_28_4.c
@@ -3,6 +3,16 @@
 //
 //输入两个数，判断这两个二进制位里不同的数有几个，如22 33 有5个
 #include "stdio.h"
+int number(int p)
+{
+    int count=0;
+    while (p){
+        p=p & (p-1);
+        count++;
+
+    }
+    return count;
+}
 int main()
 {
     int n=0;
@@ -10,11 +20,13 @@ int main()
     int count=0;
     printf("请输入两个数:\n");
     scanf("%d%d",&n,&m);
-    for (int i = 0; i < 32; ++i) {   // 一个整型数字只有32个位，所以循环32次
-         if((n>>i & 1)  !=  (m>>i &1 )){
-             count++;
-         }
-    }
+//    for (int i = 0; i < 32; ++i) {   // 一个整型数字只有32个位，所以循环32次
+//         if((n>>i & 1)  !=  (m>>i &1 )){
+//             count++;
+//         }
+//    }
+    int ret=n ^ m;
+    count=number(ret);
     printf("%d",count);
     return 0;
 }

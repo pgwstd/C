06e8732ae652ddd1ba138becbccd338571a68709
pgwstr <2712278005@qq.com>
2022-03-29 22:26:29 +0800

    3_29

--- test_3_29.c
+++ test_3_29.c
@@ -1,32 +1,36 @@
 //
 // Created by 潘光伟 on 2022/3/29.
 //
-//输入两个整数，求它们的最小公倍数
+//输入两个整数，求它们的最小公倍数和最大公约数
 #include "stdio.h"
-int min_math(int n,int m)
-{
-    if(n>m){
-        if(n%m==0)
-            printf("最小公倍数是:%d\n",n);
-        else
-            printf("最小公倍数是:%d\n",n*m);
 
-    }
-    else
-    {
-        if(m>n)
-            if(m%n==0)
-                printf("最小公倍数是:%d\n",m);
-            else
-                printf("最小公倍数是:%d\n",n*m);
+int min_math(int n, int m) {
+    if (n > m) {
+        if (n % m == 0) {
+            printf("最小公倍数是:%d\n", n);
+            printf("最大的公约数是:%d\n", m);
+        } else {
+            printf("最小公倍数是:%d\n", n * m);
+            printf("最大的公约数是:%d\n", 1);
+        }
+
+    } else {
+        if (m > n)
+            if (m % n == 0) {
+                printf("最小公倍数是:%d\n", m);
+                printf("最大公约数是:%d\n", n);
+            } else {
+                printf("最小公倍数是:%d\n", n * m);
+                printf("最大公约数是:%d\n", 1);
+            }
     }
 }
-int main()
-{
-    int i=0;
-    int j=0;
+
+int main() {
+    int i = 0;
+    int j = 0;
     printf("请输入两个整数:\n");
-    scanf("%d%d",&i,&j);
-    min_math(i,j);
+    scanf("%d%d", &i, &j);
+    min_math(i, j);
     return 0;
 }

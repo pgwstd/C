682b4fe5385de388bca6c34d5709ee9f0005d356
pgwstr <2712278005@qq.com>
2022-03-29 22:34:07 +0800

    3_29

--- test_3_29.c
+++ test_3_29.c
@@ -5,24 +5,31 @@
 #include "stdio.h"
 
 int min_math(int n, int m) {
-    if (n > m) {
-        if (n % m == 0) {
-            printf("最小公倍数是:%d\n", n);
-            printf("最大的公约数是:%d\n", m);
-        } else {
-            printf("最小公倍数是:%d\n", n * m);
-            printf("最大的公约数是:%d\n", 1);
+//    if (n > m) {
+//        if (n % m == 0) {
+//            printf("最小公倍数是:%d\n", n);
+//            printf("最大的公约数是:%d\n", m);
+//        } else {
+//            printf("最小公倍数是:%d\n", n * m);
+//            printf("最大的公约数是:%d\n", 1);
+//        }
+//
+//    } else {
+//        if (m > n)
+//            if (m % n == 0) {
+//                printf("最小公倍数是:%d\n", m);
+//                printf("最大公约数是:%d\n", n);
+//            } else {
+//                printf("最小公倍数是:%d\n", n * m);
+//                printf("最大公约数是:%d\n", 1);
+//            }
+//    }
+    int i = 0;
+    for (i = 1;; ++i) {
+        if (i * n % m == 0) {
+            printf("最小公倍数是:%d\n",i*n);
+            break;
         }
-
-    } else {
-        if (m > n)
-            if (m % n == 0) {
-                printf("最小公倍数是:%d\n", m);
-                printf("最大公约数是:%d\n", n);
-            } else {
-                printf("最小公倍数是:%d\n", n * m);
-                printf("最大公约数是:%d\n", 1);
-            }
     }
 }
 

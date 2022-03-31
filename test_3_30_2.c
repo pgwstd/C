79f15d9081e5351d283fe81a609e9505d9257386
pgwstr <2712278005@qq.com>
2022-03-31 23:15:58 +0800

    课堂练习

---
+++ test_3_30_2.c
@@ -0,0 +1,17 @@
+//
+// Created by 潘光伟 on 2022/3/30.
+//
+#include "stdio.h"
+int main()
+{
+
+    printf("%d\n",sizeof (char));  //1
+    printf("%d\n",sizeof (short ));  //2
+    printf("%d\n",sizeof (int));  //4
+    printf("%d\n",sizeof (float));  //4
+    printf("%d\n",sizeof (long));  //8
+    printf("%d\n",sizeof (long long ));  //8
+    printf("%d\n",sizeof (double)); //8
+
+    return 0;
+}

0f35e813cf85bd1cb2f46dec0924bb65d834863e
pgwstr <2712278005@qq.com>
2022-04-04 18:09:48 +0800

    韩信点兵

--- test_4_4.c
+++ test_4_4.c
@@ -14,7 +14,7 @@ int main()
     unsigned int b=0;
     unsigned int c=0;
     int i=0;
-    scanf("%d%d%d",&a,&b,&c);
+    scanf("%u%u%u",&a,&b,&c);
     for (i = 10; i <=100 ; ++i) {
         if(i%3==a && i%5==b && i%7==c){
             printf("他的兵队有%d人",i);

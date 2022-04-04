fe80b0b5cfe7999223889389ba058684a9fcecff
pgwstr <2712278005@qq.com>
2022-04-04 20:08:18 +0800

    作业更新

--- test_4_4_2.c
+++ test_4_4_2.c
@@ -6,6 +6,7 @@
 // 效率就太低了。有这么一个大平底锅，一次可以同时放入k个兰州烧饼，
 // 一分钟能做好一面。而现在有n个兰州烧饼，至少需要多少分钟才能全部做好呢？
 #include "stdio.h"
+#include "string.h"
 int main()
 {
 //    int n=0;
@@ -20,9 +21,15 @@ int main()
 //        min2 = (n - k) * 2;
 //    }
 //    printf("要多少分钟:%d\n",min1+min2);
-    unsigned int i;
-    for (i = 9; i >=0 ; i--) {
-        printf("%u\n",i);
+//    unsigned int i;
+//    for (i = 9; i >=0 ; i--) {
+//        printf("%u\n",i);
+//    }
+    char a[1000];
+    int i=0;
+    for (int i = 0; i < 1000; ++i) {
+        a[i]=-1-i;
     }
+    printf("%d\n",strlen(a));//999?  打'\0'所以就有-127+128=255（个数）
     return 0;
 }

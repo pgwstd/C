9cce4043799b0371d56334c69db8233626f973e6
pgwstr <2712278005@qq.com>
2022-04-07 20:58:14 +0800

    作业更新

--- test_4_6.c
+++ test_4_6.c
@@ -1,19 +1,69 @@
 //
 // Created by 潘光伟 on 2022/4/6.
 //
+#include <stdlib.h>
 #include "stdio.h"
+//定义一个二叉树结构
+typedef struct node {
+    int data;
+    struct node *lchild;
+    struct node *rchild;
+} Node;
+//创建一个二叉树
+Node *create_tree(Node *root) {
+    int data;
+    scanf("%d", &data);
+    if (data == -1) {
+        return NULL;
+    }
+    root = (Node *) malloc(sizeof(Node));
+    root->data = data;
+    root->lchild = create_tree(root->lchild);
+    root->rchild = create_tree(root->rchild);
+    return root;
+}
+//中序遍历
+void in_order(Node *root) {
+    if (root == NULL) {
+        return;
+    }
+    in_order(root->lchild);
+    printf("%d ", root->data);
+    in_order(root->rchild);
+}
+//先序遍历
+void pre_order(Node *root) {
+    if (root == NULL) {
+        return;
+    }
+    printf("%d ", root->data);
+    pre_order(root->lchild);
+    pre_order(root->rchild);
+}
+//后序遍历
+void post_order(Node *root) {
+    if (root == NULL) {
+        return;
+    }
+    post_order(root->lchild);
+    post_order(root->rchild);
+    printf("%d ", root->data);
+}
+//求二叉树的高度
+int height(Node *root) {
+    if (root == NULL) {
+        return 0;
+    }
+    int lheight = height(root->lchild);
+    int rheight = height(root->rchild);
+    return lheight > rheight ? lheight + 1 : rheight + 1;
+}
 int main()
 {
-    char arr[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
-    // 在数组的末尾添加一个元素
-    arr[10] = 'k';
-    // 在数组的开头添加一个元素
-    arr[-1] = 'l';
-    //输出数组
-    for (int i = 0; i < 11; i++)
-    {
-        printf("%c", arr[i]);
-    }
+    Node *root = NULL;
+    root = create_tree(root);
+    in_order(root);
+    printf("\n");
 
     return 0;
 }

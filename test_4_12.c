fcca69d8a41ee397eae76e794be6f5ada34439ce
pgwstr <2712278005@qq.com>
2022-04-12 23:12:41 +0800

    作业更新

--- test_4_12.c
+++ test_4_12.c
@@ -8,12 +8,38 @@
 //解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
 //
 #include "stdio.h"
-int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-
+int twoSum(int arr[],int getnum,int nbsize)
+{
+    int i=0;
+    int j=0;
+    while (1){
+     if(arr[i]+arr[i+1]==getnum)
+     {
+         printf("%d %d",i,i+1);
+         break;
+     }
+     else
+     {
+         i++;
+     }
+    }
 }
+
 int main()
 {
 
-   int arr[10]={0};
+    int arr[4]={2,7,11,15};
+    printf("开始遍历数组:\n");
+    for (int i = 0; i < 4; ++i) {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    int sz=sizeof(arr)/sizeof(arr[0]);
+    printf("请输入一个目标值，数组会找去和为目标值的\n");
+    printf("两个整数，并且返回数组的下标\n");
+    int target=0;
+    scanf("%d",&target);
+    printf("这两数数组的下标是:\n");
+    twoSum(arr,target,sz);
     return 0;
 }

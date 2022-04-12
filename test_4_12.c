6f7e5cde60d4dfb2208ab89ec304f9c94ab20d17
pgwstr <2712278005@qq.com>
2022-04-12 21:33:23 +0800

    作业更新

--- test_4_12.c
+++ test_4_12.c
@@ -1,11 +1,19 @@
 //
 // Created by 潘光伟 on 2022/4/12.
 //
-
+//给定一个整数数组 nums 和一个整数目标值 target，
+//请你在该数组中找出 和为目标值target的那 两个整数并返回它们的数组下标。
+//输入：nums = [2,7,11,15], target = 9
+//输出：[0,1]
+//解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+//
 #include "stdio.h"
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+
+}
 int main()
 {
 
-
+   int arr[10]={0};
     return 0;
 }

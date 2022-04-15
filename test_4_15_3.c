d7934b3262ca5b351d745934131575a40b0b6481
pgwstr <2712278005@qq.com>
2022-04-15 23:40:44 +0800

    作业更新

---
+++ test_4_15_3.c
@@ -0,0 +1,33 @@
+//
+// Created by 潘光伟 on 2022/4/15.
+//
+//计算出0-100000的水仙花数
+#include "stdio.h"
+#include "math.h"
+int main()
+{
+    int i=0;
+    for (i = 0; i <= 100000; ++i) {
+        //计算出数是几位的
+        int n=1;
+        int tmp=i;
+        while (tmp/10){
+            n++;
+            tmp=tmp/10;
+        }
+        //计算这个数各位数3次方的和
+        tmp=i;
+        int sum=0;
+        while (tmp){
+            sum+=pow(tmp%10,3);
+            tmp=tmp/10;
+        }
+        if(sum==i && i>100){
+            printf("%d ",i);
+        }
+
+
+    }
+    return 0;
+}
+

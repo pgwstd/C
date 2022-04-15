f955527f3d8c4ae8b9c7d7d1b3887d047ee6870f
pgwstr <2712278005@qq.com>
2022-04-15 22:26:53 +0800

    作业更新

--- test_4_15.c
+++ test_4_15.c
@@ -9,13 +9,13 @@ int main()
     int a=0;
     int n=0;
     int sum=0;
-    int count=0;
+    int ret=0;
     printf("请输入两个数，a是数字，n是项数:\n");
     scanf("%d%d",&a,&n);
     for (int i = 0; i < n; ++i)
     {
-            count=sum*10+a; //因为是aa是等于a乘上10加上2的得到的，所以得到这个规律
-            sum+=count;
+            ret=ret*10+a; //因为是aa是等于a乘上10加上2的得到的，所以得到这个规律
+            sum=sum+ret;
     }
     printf("它的和是:%d",sum);
     return 0;

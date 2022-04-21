7de86cf7e821e63d42b17255cdb0fab26a79ab75
pgwstr <2712278005@qq.com>
2022-04-21 22:27:50 +0800

    作业更新

--- test_4_21_2.c
+++ test_4_21_2.c
@@ -2,3 +2,15 @@
 // Created by 潘光伟 on 2022/4/21.
 //
 //找杀手，a说不是我，b说是c，c说是d，d说c在胡说，三个人说了真话，一个人说了假话，找出杀手
+#include "stdio.h"
+int main()
+{
+    char killer=0;
+    for (killer = 'A'; killer<='D' ; ++killer) {
+        if(((killer!='A') + (killer=='C') + (killer=='D') + (killer=='C')) ==3) {
+            printf("杀手是:%c\n", killer);
+        }
+    }
+
+    return 0;
+}

85ab4af07f7dd537152bf3a09e769928465cd9f2
pgwstr <2712278005@qq.com>
2022-04-26 22:16:03 +0800

    作业更新

--- test_4_26.c
+++ test_4_26.c
@@ -7,9 +7,8 @@
 #include "stdlib.h"
 int cal(int *array,int n)
 {
-    int i=0;
     int sum=0;
-    for (i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i) {
         sum+=*(array+i);
     }
     return sum;

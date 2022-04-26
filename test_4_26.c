09a05fc3950503cd5913eda9929cc6d5c1a38851
pgwstr <2712278005@qq.com>
2022-04-26 22:12:03 +0800

    作业更新

--- test_4_26.c
+++ test_4_26.c
@@ -3,6 +3,7 @@
 //
 //实现一个 int cal(int *array,int n) 的函数求出长度为 n 的数组的和
 #include "stdio.h"
+#include "stdlib.h"
 int cal(int *array,int n)
 {
     int i=0;
@@ -15,14 +16,16 @@ int cal(int *array,int n)
 int main()
 {
     int sz;
+    int* arr;
     int i=0;
     printf("请定义刚数组长度:\n");
     scanf("%d",&sz);
-    int* array[sz];
+    arr=(int *)malloc(sz*sizeof(int));//动态开辟空间n个
     printf("请输入数组的无素:\n");
     for (i = 0; i < sz; ++i) {
-        scanf("%d",array[i]);
+        scanf("%d",&arr[i]);
     }
-    cal(array,sz);
+    int sum=cal(arr,sz);
+    printf("%d",sum);
     return 0;
 }

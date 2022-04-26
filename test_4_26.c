1b0518668b08978737f83b525682bb31a86f6238
pgwstr <2712278005@qq.com>
2022-04-26 22:16:15 +0800

    作业更新

--- test_4_26.c
+++ test_4_26.c
@@ -17,12 +17,11 @@ int main()
 {
     int sz;
     int* arr;
-    int i=0;
     printf("请定义刚数组长度:\n");
     scanf("%d",&sz);
     arr=(int *)malloc(sz*sizeof(int));//动态开辟空间n个
     printf("请输入数组的无素:\n");
-    for (i = 0; i < sz; ++i) {
+    for (int i = 0; i < sz; ++i) {
         scanf("%d",&arr[i]);
     }
     int sum=cal(arr,sz);

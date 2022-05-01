be3424924b88a4e470884c3890d0a61387142aa5
pgwstr <2712278005@qq.com>
2022-05-01 23:23:36 +0800

    作业更新

--- test_5_1.c
+++ test_5_1.c
@@ -35,8 +35,38 @@
 //}
 
 
+//#include "stdio.h"
+//#include "stdlib.h"
+////打印数组
+//void print_array(int arr[],int sz)
+//{
+//    for (int i = 0; i < sz; ++i) {
+//        printf("%d ",arr[i]);
+//    }
+//    printf("\n");
+//}
+//int cmp_int(const void* e1,const void* e2)
+//{
+//    return *(int*)e1-*(int*)e2;
+//}
+//int main()
+//{
+//
+//    int arr[]={9,8,7,6,5,4,3,2,1,0};
+//    int sz=sizeof(arr)/sizeof(arr[0]);
+//    print_array(arr,sz);
+//    qsort(arr,sz,4,cmp_int);
+//    print_array(arr,sz);
+//    return 0;
+//}
+
+
 #include "stdio.h"
 #include "stdlib.h"
+struct stu{
+    char name[20];
+    int age;
+};
 //打印数组
 void print_array(int arr[],int sz)
 {
@@ -45,17 +75,19 @@ void print_array(int arr[],int sz)
     }
     printf("\n");
 }
-int cmp_int(const void* e1,const void* e2)
+int sort_by_age(const void* e1,const void* e2)
 {
-    return *(int*)e1-*(int*)e2;
+    return ((struct stu*)e1)->age - ((struct stu*)e2)->age;
+}
+void test()
+{
+    struct stu s[3]={{"张三",34},{"王五",30},{"李四",20}};
+    int sz=sizeof(s)/sizeof(s[0]);
+    qsort(s,sz,sizeof (s[0]),sort_by_age);
+//    print_array(s,sz);
 }
 int main()
 {
-
-    int arr[]={9,8,7,6,5,4,3,2,1,0};
-    int sz=sizeof(arr)/sizeof(arr[0]);
-    print_array(arr,sz);
-    qsort(arr,sz,4,cmp_int);
-    print_array(arr,sz);
+    test();
     return 0;
 }

b5e081148e8760bd168fbe83e254b5c748b839e0
pgwstr <2712278005@qq.com>
2022-05-02 21:54:15 +0800

    作业更新

--- test_5_2.c
+++ test_5_2.c
@@ -18,7 +18,8 @@ int cmp_int(const void* e1,const void* e2)
 int main()
 {
 
-    int arr[]={9,8,7,6,5,4,3,2,1,0};
+//    int arr[]={9,8,7,6,5,4,3,2,1,0};
+    int arr[]={0,1,2,3,4,5,6,7,8,9};
     int sz=sizeof(arr)/sizeof(arr[0]);
     print_array(arr,sz);
     qsort(arr,sz,4,cmp_int);

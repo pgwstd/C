a4de219135a758b1708071c2d7fa2cb0abeb2c1b
pgwstr <2712278005@qq.com>
2022-05-03 23:27:44 +0800

    作业更新

--- test_5_3_2.c
+++ test_5_3_2.c
@@ -7,9 +7,9 @@
 #include "string.h"
 void print_sort(char str[],int len)
 {
-     char *p=str;
+     char *p=str+len-1;//索引号少1，所以减去1
 //    for (int i = len-1; i < sz; ++i) {
-        while((*p+len)!='\0')
+        while(*p!='\0')
         {
             printf("%c",*p);
             p++;
@@ -21,7 +21,7 @@ int main()
     char str[100];//开辟100的字符串空间
     int len=0;
     printf("请输入字符串:\n");
-    scanf("%s",str);
+    scanf("%s",str);  //这种方法输入字符串的时候空格的时候会出错，所以不能用这种方法输入有空格的字符串
 //    int sz=strlen(str);
     printf("输入从哪个位置输出字符串:\n");
     scanf("%d",&len);

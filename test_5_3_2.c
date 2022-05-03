f6227843bca1d154279c2c722b7af461e6f2d40a
pgwstr <2712278005@qq.com>
2022-05-03 23:35:41 +0800

    作业更新

--- test_5_3_2.c
+++ test_5_3_2.c
@@ -4,7 +4,6 @@
 //复制部分字符串，如:
 //hello world! 输入7输出的是world!
 #include "stdio.h"
-#include "string.h"
 void print_sort(char str[],int len)
 {
      char *p=str+len-1;//索引号少1，所以减去1
@@ -21,8 +20,9 @@ int main()
     char str[100];//开辟100的字符串空间
     int len=0;
     printf("请输入字符串:\n");
-    scanf("%s",str);  //这种方法输入字符串的时候空格的时候会出错，所以不能用这种方法输入有空格的字符串
+//    scanf("%s",str);  //这种方法输入字符串的时候空格的时候会出错，所以不能用这种方法输入有空格的字符串
 //    int sz=strlen(str);
+    gets(str);
     printf("输入从哪个位置输出字符串:\n");
     scanf("%d",&len);
     print_sort(str,len);

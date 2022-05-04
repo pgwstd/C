e9f9fded9c0c63b7a0d27bd1bffcaec8b8344bdb
pgwstr <2712278005@qq.com>
2022-05-04 23:44:43 +0800

    作业更新

--- test_5_4_2.c
+++ test_5_4_2.c
@@ -3,21 +3,24 @@
 //
 //运用指针的方式交换数
 #include "stdio.h"
-void swap(int* m,int* n)
+void  swap(int* m,int* n)
 {
-    int *p=NULL;
-    *p=*m;
-    *m=*n;
-    *n=*p;
+    int tmp=0;
+   tmp=*m;
+   *m=*n;
+   *n=tmp;
+
+
 }
+
 int main()
 {
     int a=0;
     int b=0;
     printf("请输入两个数:\n");
     scanf("%d%d",&a,&b);
-    printf("没交换之前:%d%d",a,b);
+    printf("没交换之前:%d %d\n",a,b);
     swap(&a,&b);
-    printf("交换之后:%d%d",a,b);
+    printf("交换之后:%d %d\n",a,b);
     return 0;
 }

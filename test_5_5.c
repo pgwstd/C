2fa91648c9ae65976466a3d33b689f9f57096530
pgwstr <2712278005@qq.com>
2022-05-05 21:53:34 +0800

    作业更新

---
+++ test_5_5.c
@@ -0,0 +1,22 @@
+//
+// Created by 潘光伟 on 2022/5/5.
+//
+//利用指针遍历数组
+//键盘随机输入 6 个整数
+#include "stdio.h"
+void print_sort(int *p,int sz)
+{
+    for (int i = 0; i < sz; ++i) {
+        printf("%d ",*(p+i));
+    }
+}
+int main()
+{
+    int arr[6]={0};
+    int *str=arr;
+    for (int i = 0; i < 6; ++i) {
+        scanf("%d",(str+i));
+    }
+    print_sort(arr,6);
+    return 0;
+}

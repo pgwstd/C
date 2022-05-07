8326ff4c750746340f4ba9ec40f8f7e14cfd30ac
pgwstr <2712278005@qq.com>
2022-05-07 20:12:26 +0800

    作业更新

--- 第二阶段/test_5_6.c
+++ 第二阶段/test_5_6.c
@@ -118,14 +118,14 @@
 //}
 
 
-int main()
-{
-    int aa[2][5] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    int* ptr1 = (int*)(&aa + 1);
-    int* ptr2 = (int*)(*(aa + 1));
-    printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));
-    return 0;
-}
+//int main()
+//{
+//    int aa[2][5] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//    int* ptr1 = (int*)(&aa + 1);
+//    int* ptr2 = (int*)(*(aa + 1));
+//    printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));
+//    return 0;
+//}
 
 
 //#include <stdio.h>
@@ -140,18 +140,18 @@ int main()
 //}
 
 
-//int main()
-//{
-//    char* c[] = { "ENTER","NEW","POINT","FIRST" };
-//    char** cp[] = { c + 3,c + 2,c + 1,c };
-//    char*** cpp = cp;
-//
-//    printf("%s\n", **++cpp);
-//    printf("%s\n", *-- * ++cpp + 3);
-//    printf("%s\n", *cpp[-2] + 3);
-//    printf("%s\n", cpp[-1][-1] + 1);
-//    return 0;
-//}
+int main()
+{
+    char* c[] = { "ENTER","NEW","POINT","FIRST" };
+    char** cp[] = { c + 3,c + 2,c + 1,c };
+    char*** cpp = cp;
+
+    printf("%s\n", **++cpp);
+    printf("%s\n", *-- * ++cpp + 3);
+    printf("%s\n", *cpp[-2] + 3);
+    printf("%s\n", cpp[-1][-1] + 1);
+    return 0;
+}
 //int main()
 //{
 //    printf("demo");

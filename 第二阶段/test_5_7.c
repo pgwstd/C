0779359851b80beaa2dbee7ad2b3c0a283948ba2
pgwstr <2712278005@qq.com>
2022-05-07 22:47:30 +0800

    作业更新

--- 第二阶段/test_5_7.c
+++ 第二阶段/test_5_7.c
@@ -6,8 +6,20 @@
 // 问你能否用这个数组组成一个链表
 // 并顺序输出链表每个节点的值
 #include "stdio.h"
+#include "stdlib.h"
+typedef int ElemType;
+typedef struct LNode
+{
+    ElemType data;//定义数据域
+    struct LNode *next;//结点的指针域
+
+}LNode,*LinkList;
+
 int main()
 {
-
+    int n=0;
+    printf("请输入你要定义的数组长度:\n");
+    scanf("%d",&n);
+    Linst
     return 0;
 }

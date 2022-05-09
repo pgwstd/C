4087385b58c06aeb4f57c863401a920aadfac398
pgwstr <2712278005@qq.com>
2022-05-09 22:00:51 +0800

    作业更新

--- 第二阶段/test_5_9.c
+++ 第二阶段/test_5_9.c
@@ -4,9 +4,9 @@
 //运用函数实现数组元素左旋转与右旋转
 //输入要旋转的个数n
 //ABCD输出的左旋转2个数就是CDAB
-#include "stdio.h"
-#include "string.h"
-#include "assert.h"
+//#include "stdio.h"
+//#include "string.h"
+//#include "assert.h"
 //void String_left_rotate(char* array,int n) {
 //    assert(array);//断言
 //    int len = strlen(array);
@@ -31,55 +31,82 @@
 //    }
 //}
 
-void reverse(char* left,char* right)
-{
-    assert(left);
-    assert(right);
-    while (left < right) {
-        char tmp = *left;
-        *left = *right;
-        *right = tmp;
-        left++;
-        right--;
-    }
-}
-void String_left_rotate(char* array,int n)
-{
-    assert(array);
-    int len= strlen(array);
-    reverse(array,array+n-1);
-    reverse(array+n,array+len-1);
-    reverse(array,array+len-1);
+//void reverse(char* left,char* right)
+//{
+//    assert(left);
+//    assert(right);
+//    while (left < right) {
+//        char tmp = *left;
+//        *left = *right;
+//        *right = tmp;
+//        left++;
+//        right--;
+//    }
+//}
+//void String_left_rotate(char* array,int n)
+//{
+//    assert(array);
+//    int len= strlen(array);
+//    reverse(array,array+n-1);
+//    reverse(array+n,array+len-1);
+//    reverse(array,array+len-1);
+//
+//}
+//void String_right_rotate(char* array,int n)
+//{
+//    assert(array);
+//    int len= strlen(array);
+//    reverse(array,array+len-n-1);
+//    reverse(array+len-n,array+len-1);
+//    reverse(array,array+len-1);
+//
+//}
+//void String_print(char* array)
+//{
+//    int len= strlen(array);
+//    for (int i = 0; i < len; ++i) {
+//        printf("%c",array[i]);
+//    }
+//    printf("\n");
+//}
+//int main()
+//{
+//    char arr[10]="ABCDEF";
+//    int n=0;
+//    printf("请输入你要旋转的元素的个数:\n");
+//    scanf("%d",&n);
+//    printf("左移:\n");
+//    String_left_rotate(arr,n);
+//    String_print(arr);
+//    printf("右移:\n");
+//    String_right_rotate(arr,n);
+//    String_print(arr);
+//    return 0;
+//}
 
-}
-void String_right_rotate(char* array,int n)
-{
-    assert(array);
-    int len= strlen(array);
-    reverse(array,array+len-n-1);
-    reverse(array+len-n,array+len-1);
-    reverse(array,array+len-1);
 
-}
-void String_print(char* array)
+#include "stdio.h"
+#include "assert.h"
+//写出一个函数，判断一个字符串是否等于另外一个字符串旋转之后的字符串
+//如:s1=AABCD 和 s2=BCDAA，返回1
+int String_rotate(char* str1,char* str2)
 {
-    int len= strlen(array);
-    for (int i = 0; i < len; ++i) {
-        printf("%c",array[i]);
-    }
-    printf("\n");
+    assert(str1);
+    assert(str2);
+
 }
 int main()
 {
-    char arr[10]="ABCDEF";
-    int n=0;
-    printf("请输入你要旋转的元素的个数:\n");
-    scanf("%d",&n);
-    printf("左移:\n");
-    String_left_rotate(arr,n);
-    String_print(arr);
-    printf("右移:\n");
-    String_right_rotate(arr,n);
-    String_print(arr);
+
+    char s1="AABCD";
+    char s2="BCDAA";
+    int ret=String_rotate(s1,s2);
+    if(ret==1){
+        printf("yes\n");
+    }
+    else
+    {
+        printf("no\n");
+    }
     return 0;
 }

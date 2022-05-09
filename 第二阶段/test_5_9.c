b2dd17277ac0acae1b03eb7ba59493fec5c456b4
pgwstr <2712278005@qq.com>
2022-05-09 21:49:17 +0800

    作业更新

--- 第二阶段/test_5_9.c
+++ 第二阶段/test_5_9.c
@@ -7,28 +7,57 @@
 #include "stdio.h"
 #include "string.h"
 #include "assert.h"
-void String_left_rotate(char* array,int n) {
-    assert(array);//断言
-    int len = strlen(array);
-    for (int i = 0; i < n; ++i) {  //移多少次头元素
-        char tmp = *array;
-        for (int j = 0; j < len -  1; ++j) { //向前移动
-            *(array + j) = *(array + j + 1);
-        }
-        *(array + len - 1) = tmp;  //把头元素给数组尾部
+//void String_left_rotate(char* array,int n) {
+//    assert(array);//断言
+//    int len = strlen(array);
+//    for (int i = 0; i < n; ++i) {  //移多少次头元素
+//        char tmp = *array;
+//        for (int j = 0; j < len -  1; ++j) { //向前移动
+//            *(array + j) = *(array + j + 1);
+//        }
+//        *(array + len - 1) = tmp;  //把头元素给数组尾部
+//    }
+//}
+//void String_right_rotate(char* array,int n)
+//{
+//    assert(array);//断言
+//    int len = strlen(array);
+//    for (int i = 0; i < n; ++i) {  //移多少次尾元素
+//        char tmp = *(array+len-1);
+//        for (int j = len-1; j >= 0; --j) { //向后移动
+//            *(array + j) = *(array + j - 1);
+//        }
+//        *array  = tmp;  //把尾元素给数组头部
+//    }
+//}
+
+void reverse(char* left,char* right)
+{
+    assert(left);
+    assert(right);
+    while (left < right) {
+        char tmp = *left;
+        *left = *right;
+        *right = tmp;
+        left++;
+        right--;
     }
+}
+void String_left_rotate(char* array,int n)
+{
+    int len= strlen(array);
+    reverse(array,array+n-1);
+    reverse(array+n,array+len-1);
+    reverse(array,array+len-1);
+
 }
 void String_right_rotate(char* array,int n)
 {
-    assert(array);//断言
-    int len = strlen(array);
-    for (int i = 0; i < n; ++i) {  //移多少次尾元素
-        char tmp = *(array+len-1);
-        for (int j = len-1; j >= 0; --j) { //向后移动
-            *(array + j) = *(array + j - 1);
-        }
-        *array  = tmp;  //把尾元素给数组头部
-    }
+   int len= strlen(array);
+    reverse(array,array+len-n-1);
+    reverse(array+len-n,array+len-1);
+    reverse(array,array+len-1);
+
 }
 void String_print(char* array)
 {

2e59f3828c73983098deb5c0d05af9c2457a039a
pgwstr <2712278005@qq.com>
2022-05-11 20:48:37 +0800

    作业更新

---
+++ 第二阶段/test_5_11.c
@@ -0,0 +1,27 @@
+//
+// Created by 潘光伟 on 2022/5/11.
+//
+//strcpy函数实现
+//#include "stdio.h"
+//#include "string.h"
+//int main() {
+//
+//    char arr[] = {'a','b','c'};
+//    char *p = "hello";
+//    int len=sizeof(arr)/sizeof (arr[0]);
+//    int len=strlen(arr);
+//    strcpy(arr,p);
+//    printf("%s",arr);
+//    printf("%d",len);
+//    return 0;
+//}
+#include "stdio.h"
+#include "string.h"
+int main()
+{
+    char arr[20]="abc";
+    char arr2[]="efg";
+    strcat(arr,arr2);
+    printf("%s",arr);
+    return 0;
+}

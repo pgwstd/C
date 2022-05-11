8cfb445b30e67da6cf76854f494fcccdea8636f8
pgwstr <2712278005@qq.com>
2022-05-11 11:11:31 +0800

    作业更新

--- 第二阶段/test_5_10.c
+++ 第二阶段/test_5_10.c
@@ -1,18 +1,47 @@
 //
 // Created by 潘光伟 on 2022/5/10.
 //
-//strlen函数实现
+//strlen函数递归实现
 #include "stdio.h"
 #include "assert.h"
-int strlen_sort(const char* array)
+//strlen函数计数实现
+//int strlen_sort(const char* array)
+//{
+//    int count=0;
+//    assert(array);
+//    while (*array  != '\0'){
+//        count++;
+//        array++;
+//    }
+//    return count;
+//}
+//strlen函数递归实现
+//int strlen_sort(const char* array)
+//{
+//
+//    assert(array);
+//    if(*array == '\0'){
+//        return 0;
+//    }
+//    else {
+//        return strlen_sort(++array)+1;
+//    }
+//}
+
+
+
+//strlen函数指针相减实现
+int strlen_sort( char* array)
 {
-    int count=0;
+
     assert(array);
-    while (*array  != '\0'){
-        count++;
-        array++;
+    char *left=array;
+    char *right=array;
+    while(*right != '\0')
+    {
+        right++;
     }
-    return count;
+    return right-left;
 }
 int main()
 {

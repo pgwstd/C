80e93eae19decd616c2b6db4e7c1fa0e45e3bc7d
pgwstr <2712278005@qq.com>
2022-05-14 22:04:28 +0800

    作业更新

--- 第二阶段/test_5_14_2.c
+++ 第二阶段/test_5_14_2.c
@@ -1,10 +1,26 @@
 //
 // Created by 潘光伟 on 2022/5/14.
 //
+//memcpy内存函数实现
 #include "stdio.h"
+#include "string.h"
+#include "assert.h"
+void* my_memcpy(void* dest,const void*src,int num)
+{
+    void* ret=dest;
+    while (num--){
+        *(char*)dest=*(char*)src;
+        dest=(char*)dest+1;
+        src=(char*)src+1;
+    }
+    return ret;
+}
 int main()
 {
 
-    printf("%d",123);
+    char arr1[10]={1,2,3,4,5,6,7,8,9,10};
+    char arr2[10]={1};
+    my_memcpy(arr1,arr2,10);
+    printf("%s",arr1);
     return 0;
 }

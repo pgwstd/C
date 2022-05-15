d31e0c38c16e65586a6eb46e09a2cf08e39e237c
pgwstr <2712278005@qq.com>
2022-05-15 22:06:07 +0800

    作业更新

--- 第二阶段/test_5_15.c
+++ 第二阶段/test_5_15.c
@@ -2,13 +2,22 @@
 // Created by 潘光伟 on 2022/5/15.
 //
 #include "stdio.h"
-struct book{
-    char id[20];
-    int pir;
-    char pd[10];
-}b1;
-int main()
-{
-    printf("%d",sizeof(b1));
+
+struct book {
+//    char id;
+//    char pd;
+//    int pir;
+    double d;//8      8+16+8=32
+    char c;//1
+    int i;//4
+};
+struct book2{
+    char c1;
+    struct book b1;
+    double d;
+};
+int main() {
+    struct book2 b2;
+    printf("%d", sizeof(b2));
     return 0;
 }

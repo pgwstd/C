3dc035cb62b1dc735388833ff89f091d7da618fe
pgwstr <2712278005@qq.com>
2022-05-15 19:07:53 +0800

    作业更新

--- 第二阶段/test_5_14_2.c
+++ 第二阶段/test_5_14_2.c
@@ -16,11 +16,34 @@ void* my_memcpy(void* dest,const void* src,size_t num)
     }
     return ret;
 }
+void* my_memmove(void* dest,const void* src,size_t num)
+{
+    assert(dest && src);
+    void* ret=dest;
+    if(dest < src){
+        while (num--) {
+            *(char *) dest = *(char *) src;
+            dest = (char *) dest + 1;
+            src = (char *) src + 1;
+        }
+    }
+    else
+    {
+        while (num--) {
+            *((char *) dest + num) = *((char *) src + num);
+        }
+    }
+    return ret;
+}
 int main()
 {
 
-    char arr1[10]={1,2,3,4,5,6,7,8,9,10};
-    char arr2[10]={ 0 };
-    my_memcpy(arr2,arr1,10);
+    int arr1[10]={1,2,3,4,5,6,7,8,9,10};
+//    int arr2[10]={ 1 };
+//    my_memcpy(arr1+2,arr1,20);
+    my_memmove(arr1+2,arr1,20);
+    for (int i = 0; i < 10; ++i) {
+        printf("%d\n",arr1[i]);
+    }
     return 0;
 }

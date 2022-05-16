3a854d82661ec3dae75b79e423aad1dbc2791853
pgwstr <2712278005@qq.com>
2022-05-16 13:59:35 +0800

    作业更新

--- 第一阶段/test_2_28_5.c
+++ 第一阶段/test_2_28_5.c
@@ -3,35 +3,31 @@
 //
 //在一组数组中使用二分查找，查找到想要数的下标
 #include "stdio.h"
-int search(int a[],int k,int n)
-{
-    int left=0;
-    int right=n-1;
-    while (left<=right){
-        int mid=(left+right)/2;
-        if(a[mid]>k){
-            right=mid-1;
-        }
-        else if(a[mid]<k){
-            left=mid+1;
-        }
-        else
+
+int search(int a[], int k, int n) {
+    int left = 0;
+    int right = n - 1;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (a[mid] > k) {
+            right = mid - 1;
+        } else if (a[mid] < k) {
+            left = mid + 1;
+        } else
             return mid;
     }
     return -1;
 
 }
-int main()
-{
-    int  arr[]={1,2,3,4,5,6,7,8,9};
-    int key=7;  //要找的数字
-    int size=sizeof(arr)/sizeof(arr[0]);//计算出这个数组的下标个数
-    int z=search(arr,key,size);
-    if(z!=-1){
-        printf("找到了，下标是：%d",z);
-    }
-    else
-    {
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int key = 7;  //要找的数字
+    int size = sizeof(arr) / sizeof(arr[0]);//计算出这个数组的下标个数
+    int z = search(arr, key, size);
+    if (z != -1) {
+        printf("找到了，下标是：%d", z);
+    } else {
         printf("找不到");
     }
     return 0;
--- 第二阶段/test_5_16.c
+++ 第二阶段/test_5_16.c
@@ -2,36 +2,30 @@
 // Created by 潘光伟 on 2022/5/16.
 //
 #include "stdio.h"
-int find_sort(int ptr[],int k,int sz)
-{
-    int left=0;
-    int right=sz-1;
-    while (left <= right)
-    {
-        int mid=(left+right)/2;
-        if(k < ptr[mid])
-        {
-              right=mid-1;
-        }
-        else if(k > ptr[mid])
-        {
-           left=mid+1;
-        }
-        else
-        {
+
+int find_sort(int ptr[], int k, int sz) {
+    int left = 0;
+    int right = sz - 1;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (k < ptr[mid]) {
+            right = mid - 1;
+        } else if (k > ptr[mid]) {
+            left = mid + 1;
+        } else {
             return mid;
         }
     }
     return -1;
 }
-int main()
-{
 
-    int  arr[]={1,2,3,4,5,6,7,8,9,10};
-    int kil=0;
-    int size=sizeof(arr)/sizeof(arr[0]);
-    scanf("%d",&kil);
-    int ret=find_sort(arr,kil,size);
-    printf("下标是:%d",ret);
+int main() {
+
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int kil = 0;
+    int size = sizeof(arr) / sizeof(arr[0]);
+    scanf("%d", &kil);
+    int ret = find_sort(arr, kil, size);
+    printf("下标是:%d", ret);
     return 0;
 }

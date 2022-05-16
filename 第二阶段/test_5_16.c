6c19d977c6013a2d9c603b9c3bbcf0e9d7e6ec34
pgwstr <2712278005@qq.com>
2022-05-16 12:45:13 +0800

    作业更新

---
+++ 第二阶段/test_5_16.c
@@ -0,0 +1,23 @@
+//
+// Created by 潘光伟 on 2022/5/16.
+//
+#include "stdio.h"
+int find_sort(int *ptr,int k,int sz)
+{
+    int* left=ptr;
+    int* right=ptr+sz-1;
+    while (left < right){
+        if(*ptr < )
+    }
+}
+int main()
+{
+
+    int  arr[]={1,2,3,4,5,6,7,8,9,10};
+    int kil=0;
+    int size=sizeof(arr)/sizeof(arr[0]);
+    printf("请输入你要查找的数:\n");
+    scanf("%d",&kil);
+    find_sort(arr,kil,size);
+    return 0;
+}

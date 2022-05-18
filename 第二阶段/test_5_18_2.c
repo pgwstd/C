fdef82fbf27d6b9a3cc2d984e58112b2631c678d
pgwstr <2712278005@qq.com>
2022-05-18 22:22:10 +0800

    作业更新

--- 第二阶段/test_5_18_2.c
+++ 第二阶段/test_5_18_2.c
@@ -1,42 +1,54 @@
 //
 // Created by 潘光伟 on 2022/5/18.
 //
-#include "stdio.h"
-//计算大小端
-int Atom_sys()
-{
-    int a=1;
-    if((*(char*)&a) == 1)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
-//使用共用体计算
-int Atom_sys2()
-{
-    union U{
-        char i;
-        int a;
-    }u;
-    u.a=1;
-    return u.i;
-}
-int main()
-{
-    int ret=Atom_sys2();
-    if(ret == 1){
-        printf("小端\n");
-    }
-    else
-    {
-        printf("大端\n");
-    }
-
-    return 0;
-}
+//#include "stdio.h"
+////计算大小端
+//int Atom_sys()
+//{
+//    int a=1;
+//    if((*(char*)&a) == 1)
+//    {
+//        return 1;
+//    }
+//    else
+//    {
+//        return 0;
+//    }
+//}
+////使用共用体计算
+//int Atom_sys2()
+//{
+//    union U{
+//        char i;
+//        int a;
+//    }u;
+//    u.a=1;
+//    return u.i;
+//}
+//int main()
+//{
+//    int ret=Atom_sys2();
+//    if(ret == 1){
+//        printf("小端\n");
+//    }
+//    else
+//    {
+//        printf("大端\n");
+//    }
+//
+//    return 0;
+//}
 
 //计算共用体的大小
+#include "stdio.h"
+union U{
+    char arr[5];
+    int a;
+}u;
+int main()
+{
+
+    printf("%d\n",sizeof(u));
+    printf("%d\n",sizeof(union U));
+    return 0;
+}

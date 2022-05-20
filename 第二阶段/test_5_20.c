b0e003a1898901088f461eebc331393c95793ec2
pgwstr <2712278005@qq.com>
2022-05-20 21:58:39 +0800

    作业更新

--- 第二阶段/test_5_20.c
+++ 第二阶段/test_5_20.c
@@ -18,27 +18,48 @@
 //    p = NULL; //指针置为空
 //    return 0;
 //}
+//#include "stdio.h"
+//#include "stdlib.h"
+//int main()
+//{
+//    int* p=(int*)(calloc(10,sizeof(int)));
+//    if(p == NULL){
+//        printf("分配失败!");
+//        return 1;
+//    }
+//    for (int i = 0; i < 10; ++i) {
+//        *(p+i)=i;
+//    }
+//    int* ptr=(int*) realloc(p,sizeof(int));
+//    if(ptr != NULL)
+//    {
+//        p=ptr;
+//    }
+//    for (int i = 0; i < 20; ++i) {
+//        printf("%d ",*(p+i));
+//    }
+//    free(p);
+//    p = NULL;
+//    return 0;
+//}
 #include "stdio.h"
 #include "stdlib.h"
 int main()
 {
-    int* p=(int*)(calloc(10,sizeof(int)));
+    
+    int* p=(int*) realloc(NULL,10*sizeof(int));
     if(p == NULL){
-        printf("分配失败!");
+        printf("分配失败！");
         return 1;
     }
     for (int i = 0; i < 10; ++i) {
         *(p+i)=i;
     }
-    int* ptr=(int*) realloc(p,sizeof(int));
-    if(ptr != NULL)
-    {
-        p=ptr;
-    }
-    for (int i = 0; i < 20; ++i) {
-        printf("%d ",*(p+i));
+    for (int i = 0; i < 10; ++i) {
+        printf("%d ",p[i]);
     }
     free(p);
     p = NULL;
+
     return 0;
 }

f42e5caca2bef826815ce21d385137e2a8c55bb1
pgwstr <2712278005@qq.com>
2022-05-20 21:55:33 +0800

    作业更新

--- 第二阶段/test_5_20.c
+++ 第二阶段/test_5_20.c
@@ -28,7 +28,17 @@ int main()
         return 1;
     }
     for (int i = 0; i < 10; ++i) {
+        *(p+i)=i;
+    }
+    int* ptr=(int*) realloc(p,sizeof(int));
+    if(ptr != NULL)
+    {
+        p=ptr;
+    }
+    for (int i = 0; i < 20; ++i) {
         printf("%d ",*(p+i));
     }
+    free(p);
+    p = NULL;
     return 0;
 }

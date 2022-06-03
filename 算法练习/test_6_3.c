4c08853068569aaa3113da88dd35a5400e09521d
panguangwei <2712278005@qq.com>
2022-06-03 12:31:16 +0800

    算法练习

---
+++ 算法练习/test_6_3.c
@@ -0,0 +1,24 @@
+//
+// Created by 潘光伟 on 2022/6/3.
+//
+//算法练习
+//输入一个字符，然后输出以下效果
+//  #
+// ###
+//#####
+// ###
+//  #
+#include "stdio.h"
+int main()
+{
+
+    char str={0};
+    printf("请输入你要打印的字符:\n");
+    scanf("%c",&str);
+    //打印上半部分
+    for(int i=0;;i++){
+        printf(" ");
+    }
+    //打印下部分
+    return 0;
+}

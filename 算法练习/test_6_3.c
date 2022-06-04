d5be5330cc76f025e867e95a1ef2276231cdcd66
panguangwei <2712278005@qq.com>
2022-06-04 23:41:23 +0800

    算法练习

--- 算法练习/test_6_3.c
+++ 算法练习/test_6_3.c
@@ -15,11 +15,25 @@ int main()
     char ch=0;
     printf("请输入你要打印的字符:\n");
     scanf("%c",&ch);
+    int n=3;
     //打印上半部分
-    for(int i=0;i<3;i++){
-        printf(" ");
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 0; j < n-i; j++)
+            printf(" ");    //打印空格
+        for(int j = 0; j < 2*i-1; j++)
+            printf("%c", ch);   //打印字符
+        printf("\n");
+    }
+    //打印下半部分
+    for(int i = 1; i < n; i++)
+    {
+        for(int j = 0; j < i; j++)
+            printf(" ");
+        for(int j = 0; j < 2*(n-i)-1; j++)
+            printf("%c", ch);
+        printf("\n");
     }
 
-    //打印下部分
     return 0;
 }

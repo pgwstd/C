d60d5a19f818680c26b5761c0316ea40d11a1fac
panguangwei <2712278005@qq.com>
2022-06-08 17:40:16 +0800

    算法练习

--- 算法练习/test_6_6.cpp
+++ 算法练习/test_6_6.cpp
@@ -2,6 +2,7 @@
 #include "iostream"
 #include "string"
 #define ERROR -1
+#define OK 1
 #define MAXSIZE 100
 using namespace std;
 
@@ -23,91 +24,64 @@ typedef struct
 //定义栈结点类型
 typedef struct
 {
-    BiTNode *base;
-    BiTNode *top;
+    BiTree *base;
+    BiTree *top;
     int stacksize;
 }BiTreeStack;
 
 //初始化字符栈
 void InitStack(SqStack &S)
 {
-
-S.base = new char[MAXSIZE];
-if (!S.base)
-{
-cout << "分配内存失败" << endl;
-exit(ERROR);
-}
-S.top = S.base;
-S.stacksize = MAXSIZE;
+    S.base = new char[MAXSIZE];
+    if (!S.base) exit(ERROR);
+    S.top = S.base;
+    S.stacksize = MAXSIZE;
 }
 
 //初始化二叉树栈
 void InitBiTreeStack(BiTreeStack &S)
 {
-S.base = new BiTNode[MAXSIZE];
-if (!S.base)
-{
-cout << "分配内存失败" << endl;
-exit(ERROR);
-}
-S.top = S.base;
-S.stacksize = MAXSIZE;
+    S.base = new BiTree[MAXSIZE];
+    if (!S.base) exit(ERROR);
+    S.top = S.base;
+    S.stacksize = MAXSIZE;
 }
 
 //字符栈入栈
 void Push(SqStack &S, char e)
 {
-if (S.top - S.base >= S.stacksize)
-{
-cout << "栈满" << endl;
-exit(ERROR);
-}
-*S.top++ = e;
+    if (S.top - S.base == S.stacksize) return;
+    *S.top = e;
+    S.top++;
 }
 
 //树栈入栈
 void PushBiTree(BiTreeStack &S, BiTree e)
 {
-if (S.top - S.base >= S.stacksize)
-{
-cout << "栈满" << endl;
-exit(ERROR);
-}
-*S.top++ = e;
+    if (S.top - S.base == S.stacksize) return;
+    *S.top = e;
+    S.top++;
 }
 
 //字符栈出栈
 void Pop(SqStack &S, char &e)
 {
-if (S.top == S.base)
-{
-cout << "栈空" << endl;
-exit(ERROR);
-}
-e = *--S.top;
+    if (S.top == S.base) return;
+    e = *--S.top;
 }
 
 //树栈出栈
 void PopBiTree(BiTreeStack &S, BiTree &e)
 {
-if (S.top == S.base)
-{
-cout << "栈空" << endl;
-exit(ERROR);
-}
-e = *--S.top;
+    if (S.top == S.base) return;
+    e = *--S.top;
 }
 
 //字符栈得到栈顶元素
-void GetTop(SqStack &S, char &e)
+char GetTop(SqStack &S)
 {
-if (S.top == S.base)
-{
-cout << "栈空" << endl;
-exit(ERROR);
-}
-e = *(S.top - 1);
+    if (S.top == S.base) exit(OK);
+    return *(S.top - 1);
 }
 
 
@@ -203,59 +177,59 @@ int GetValue(char theta,int a,int b)
 }
 
 //简单的二叉树创立
-void GreateExpTree(BiTree &T,BiTree a,BiTree b,char ch)
+void CreateExpTree(BiTree &T,BiTree a,BiTree b,char ch)
 {
-T = new BiTNode;
-T->lchild = a;
-T->rchild = b;
-T->data = ch;
+    T = new BiTNode;
+    T->lchild = a;
+    T->rchild = b;
+    T->data = ch;
 }
 
 void InitExpTree(BiTree &T)
 {
-SqStack OPTR;
-BiTreeStack EXPT;
-char ch,theta,x;
-BiTree a,b;
-InitStack(OPTR);
-Push(OPTR,'#');
-InitBiTreeStack(EXPT);
-cin>>ch;
-while(ch != '#' || GetTop(OPTR) != '#')
-{
-if (!In(ch))
-{
-GreateExpTree(a,NULL,NULL,ch);
-PushBiTree(EXPT,a);
-}
-else
-{
-switch (Precede(GetTop(OPTR),ch))
-{
-case '<':
-Push(OPTR,ch);
-cin>>ch;
-break;
-case '=':
-Pop(OPTR,x);
-cin>>ch;
-break;
-case '>':
-Pop(OPTR,theta);
-PopBiTree(EXPT,b);
-PopBiTree(EXPT,a);
-GreateExpTree(T,a,b,theta);
-PushBiTree(EXPT,T);
-break;
-}
-}
-}
+    SqStack OPTR;
+    BiTreeStack EXPT;
+    char ch,theta,x;
+    BiTree a,b;
+    InitStack(OPTR);
+    Push(OPTR,'#');
+    InitBiTreeStack(EXPT);
+    cin>>ch;
+    while(ch != '#' || GetTop(OPTR) != '#')
+    {
+        if (!In(ch))
+        {
+            CreateExpTree(T,NULL,NULL,ch);
+            PushBiTree(EXPT,T);
+            cin>>ch;
+        }
+        else
+            switch (Precede(GetTop(OPTR),ch))
+            {
+                case '<':
+                    Push(OPTR,ch);
+                    cin>>ch;
+                    break;
+                case '=':
+                    Pop(OPTR,x);
+                    cin>>ch;
+                    break;
+                case '>':
+                    Pop(OPTR,theta);
+                    PopBiTree(EXPT,b);
+                    PopBiTree(EXPT,a);
+                    CreateExpTree(T,a,b,theta);
+                    PushBiTree(EXPT,T);
+                    break;
+            }
+
+    }
 }
 
 int EvaluateExpTree(BiTree T)
 {
     int lvalue,rvalue;
-    if (!T->lchild && !T->rchild)
+    if (T->lchild==NULL && T->rchild==NULL)
         return T->data - '0';
     else
     {
@@ -271,6 +245,8 @@ int main()
     cout<< "请输入算术表达式，并以#结束，中间计算过程要是1位数："<<endl;
     BiTree T;
     InitExpTree(T);
+    cout<< "运算结果是："<<endl;
     cout<<EvaluateExpTree(T)<<endl;
 
 }
+

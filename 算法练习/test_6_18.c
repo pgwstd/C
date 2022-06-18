152bd7eb6ea714660479295f8f48254c8f223340
panguangwei <2712278005@qq.com>
2022-06-18 14:35:46 +0800

    算法练习

--- 算法练习/test_6_18.c
+++ 算法练习/test_6_18.c
@@ -2,25 +2,47 @@
 // Created by 潘光伟 on 2022/6/18.
 //
 #include "stdio.h"
-int main()
+//冒泡排序
+void bbubble_sort(int a[], int n)
 {
-    //冒泡排序
-    int a[10] = {1,2,3,4,5,6,7,8,9,10};
-    int i,j,temp;
-    for(i=0;i<10;i++)
+    int i, j, temp;
+    for (i = 0; i < n; i++)
     {
-        for(j=0;j<10-i-1;j++)
+        for (j = 0; j < n - i - 1; j++)
         {
-            if(a[j]<a[j+1])
+            if (a[j] < a[j + 1])
             {
                 temp = a[j];
-                a[j] = a[j+1];
-                a[j+1] = temp;
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
             }
         }
     }
-    for(i=0;i<10;i++)
+}
+//输出数组
+void print_array(int a[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
     {
-        printf("%d ",a[i]);
+        printf("%d ", a[i]);
     }
+    printf("\n");
+}
+int main()
+{
+
+    int a[] = {0};
+    //从键盘输入数组
+    int n;
+    printf("请输入数组的长度：");
+    scanf("%d", &n);
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("请输入第%d个数：", i + 1);
+        scanf("%d", &a[i]);
+    }
+    bbubble_sort(a, n);
+    print_array(a, n);
 }

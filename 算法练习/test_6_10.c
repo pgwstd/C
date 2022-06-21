6276947737339ee4b81c10853ccd39b170d2fb01
panguangwei <2712278005@qq.com>
2022-06-21 18:28:17 +0800

    算法更新

---
+++ 算法练习/test_6_10.c
@@ -0,0 +1,11 @@
+//
+// Created by 潘光伟 on 2022/6/20.
+//
+#include "stdio.h"
+int main()
+{
+    int a=0;
+    int b=0;
+    scanf("%d%d",&a,&b);
+    printf("%d",a+b);
+}
---
+++ 算法练习/test_6_21.c
@@ -0,0 +1,8 @@
+//
+// Created by 潘光伟 on 2022/6/21.
+//
+#include "stdio.h"
+int main()
+{
+    printf("123123");
+}

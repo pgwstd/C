fb251f86f7a4ee303c55d0a2675598c45bcdd046
panguangwei <2712278005@qq.com>
2022-06-24 23:40:48 +0800

    算法练习

--- 算法练习/test_6_24.c
+++ 算法练习/test_6_24.c
@@ -162,7 +162,7 @@ void SeqListPrint(SeqList *L)
 //查找表中指定位置的元素
 int SeqListGetElem(SeqList *L, int i)
 {
-    assert(L->size >= i && i >= 0);
+    assert(L->size >  i && i >= 0);
     if (i <= L->size || i > 0)
     {
         printf("位于表%d的位置的元素是:%d \n", i, L->Data[i - 1]);
@@ -197,7 +197,7 @@ int SeqListSearchElem(SeqList *L, SLData x)
 //删除指定位置的元素
 void SeqListSiteElem(SeqList *L, int i)
 {
-    assert(L->size >= i && i >= 0);
+    assert(L->size > i && i >= 0);
     if (i <= L->size || i >= 0)
     {
         i = i - 1;

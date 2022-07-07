4ca1d43c952cf126894ff7929ea2d276438af9b0
pgwstr <2712278005@qq.com>
2022-07-07 21:14:41 +0800

    算法练习

---
+++ 算法练习/test_7_7.c
@@ -0,0 +1,28 @@
+//
+// Created by 潘光伟 on 2022/7/7.
+//
+//链表中的相邻间的两个内容交换
+#include "stdio.h"
+#include "stdlib.h"
+typedef int ListData;
+//定义链表
+typedef struct LinkList
+{
+    ListData data;
+    int size;
+    int leng;
+}LinkList;
+
+
+
+
+
+
+
+int main()
+{
+
+
+
+    return 0;
+}

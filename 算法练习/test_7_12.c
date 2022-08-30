a9ae2566efc97cc8a7f2a3693f3d799989132d5c
pgwstr <2712278005@qq.com>
2022-08-30 16:16:46 +0800

    作业

--- 算法练习/test_7_12.c
+++ 算法练习/test_7_12.c
@@ -4,9 +4,7 @@
 #include "stdio.h"
 int main()
 {
-    //
-    // Created by 潘光伟 on 2022/7/29.
-    //
+
     int arr[] = {1,3,4,5,6,7,8};
     int size = sizeof(arr) / sizeof(arr[0]);
     for (int i = 0; i < size; ++i)

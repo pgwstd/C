c714d190def4ed94ed6f9f1d9c8b6e683cc74cda
pgwstr <2712278005@qq.com>
2022-09-02 20:59:17 +0800

    练习

--- 算法练习/test_9_2.c
+++ 算法练习/test_9_2.c
@@ -4,24 +4,30 @@
 //数组int A[10]数组元素升序
 #include "stdio.h"
 
-int Num_short(int arr[],int sz){
+void Num_Rise(int arr[], int sz)
+{
+
+    int tmp = 0;
     for (int i = 0; i < sz; ++i)
     {
-        if(arr[i] > arr[i+1]){
-            int tmp;
-            arr[i] = tmp;
-            arr[i] = arr[i+1];
-            arr[i+1] = tmp;
+        if (arr[i] > arr[i + 1])
+        {
+            tmp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = tmp;
         }
-        return arr;
     }
 }
 
 
 int main()
 {
-    int A[10] = {6,3,2,1,9,5,2,4,7,10};
-    int size = sizeof (A) / sizeof (A[0]);
-    printf("%d",Num_short(A,size));
+    int A[10] = {6, 3, 2, 1, 9, 5, 2, 4, 7, 10};
+    int size = sizeof(A) / sizeof(A[0]);
+    Num_Rise(A, size);
+    for (int i = 0; i < size; ++i)
+    {
+        printf("%d ", A[i]);
+    }
     return 0;
 }

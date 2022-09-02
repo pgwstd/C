0954ab2fe65df6f4424b29af252d66091035c058
pgwstr <2712278005@qq.com>
2022-09-03 01:14:00 +0800

    练习

--- 算法练习/test_9_2.c
+++ 算法练习/test_9_2.c
@@ -7,27 +7,43 @@
 void Num_Rise(int *arr, int sz)
 {
 
-    int tmp = 0;
+    int tmp=0;
+    for (int i = 0; i < sz - 1; ++i)
+    {
+        for (int j = 0; j < sz - 1; ++j)
+        {
+            if(*(arr + j) > *(arr + j + 1)){
+                tmp = *(arr + j);
+                *(arr + j) = *(arr + j + 1);
+                *(arr + j + 1) = tmp;
+            }
+        }
+    }
+
+}
+
+void Print_Num(int *arr, int sz)
+{
     for (int i = 0; i < sz; ++i)
     {
-        if (arr[i] > arr[i + 1])
-        {
-            tmp = arr[i];
-            arr[i] = arr[i + 1];
-            arr[i + 1] = tmp;
-        }
+        printf("%d ", *(arr+i));
     }
 }
 
-
 int main()
 {
-    int A[10] = {6, 3, 2, 1, 9, 5, 2, 4, 7, 10};
+    int A[10] = { 0 };
     int size = sizeof(A) / sizeof(A[0]);
-    Num_Rise(A, size);
+    printf("请输入十个数进行比较:\n");
     for (int i = 0; i < size; ++i)
     {
-        printf("%d ", A[i]);
+        scanf("%d",&A[i]);
     }
+    printf("没交换前:\n");
+    Print_Num(A,size);
+    printf("\n");
+    Num_Rise(A, size);
+    printf("交换后:\n");
+    Print_Num(A, size);
     return 0;
 }
--- 算法练习/test_9_2_2.c
+++ 算法练习/test_9_2_2.c
@@ -1,5 +1,29 @@
 //
 // Created by 潘光伟 on 2022/9/2.
 //
-//在32位系统中，实现 unsigned short int A[10]数组的求和，及均值计算。(其中 unsigned int为32位
-//数据，unsigned short int为16位数据。)
+//在32位系统中，实现 unsigned short int A[10]数组的求和，
+//及均值计算。(其中 unsigned int为32位数据，unsigned short int为16位数据。)
+#include "stdio.h"
+
+short int Arr_sum(short int arr[])
+{
+    int sum = 0;
+    for (int i = 0; i < 10; ++i)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main()
+{
+    unsigned short int A[10] = { 0 };
+    printf("请输入十个数:\n");
+    for (int i = 0; i < 10; ++i)
+    {
+        scanf("%hd",&A[i]);
+    }
+    short int sum = Arr_sum(A);
+    printf("和为:%d",sum);
+    return 0;
+}

dfcde41f497fe9313acbc619acf9f6a80092eb0e
pgwstr <2712278005@qq.com>
2022-10-22 13:26:55 +0800

    练习

--- 算法练习/test_10_20_2.c
+++ 算法练习/test_10_20_2.c
@@ -4,45 +4,61 @@
 #include<stdio.h>
 #include<signal.h>
 #include<unistd.h>
+#include<sys/types.h>
 #include<sys/wait.h>
 #include<stdlib.h>
-int wait_flag;
+void waiting();
 void stop();
-int main()
-{
-    int pid1,pid2;
-    signal(3,stop);
+int wait_logding;
+int main(){
+    int pid1, pid2;
     while((pid1 = fork()) == -1);
     if(pid1 > 0){
         while((pid2 = fork()) == -1);
         if(pid2 > 0){
-            wait_flag = 1;
-            signal(2,stop);
-            sleep(5);
-            kill(pid1,16);
-            kill(pid2,17);
+            wait_logding = 1;
+            signal(SIGINT,stop);
+            waiting();
+            sleep(1);
+            kill(pid1, 16);
+            sleep(1);
+            kill(pid2, 17);
             wait(0);
             wait(0);
-            printf("\n parent process is killed!!\n");
+            printf("parent process is killed!\n");
             exit(0);
         }
-        else
-        {
-            wait_flag = 1;
+        else{
+            signal(SIGINT,SIG_IGN);
+            signal(SIGQUIT,SIG_IGN);
             signal(17,stop);
-            printf("\n child process 2 is kille by parent!!\n");
+            wait_logding=1;
+            waiting();
+            lockf(1,1,0);
+            printf("child process 2 is killed by parent!\n");
+            lockf(1,0,0);
             exit(0);
+
         }
     }
     else{
-        wait_flag = 1;
+        signal(SIGINT,SIG_IGN);
+        signal(SIGQUIT,SIG_IGN);
         signal(16,stop);
-        printf("\n child process 1 is kille by parent!!\n");
+        wait_logding=1;
+        waiting();
+        lockf(1,1,0);
+        printf("child process 1 is killed by parent!\n");
+        lockf(1,0,0);
         exit(0);
     }
 }
-void stop()
-{
-    wait_flag = 0;
+
+void waiting(){
+    while(wait_logding != 0);
+}
+
+void stop(){
+    wait_logding = 0;
 }
 

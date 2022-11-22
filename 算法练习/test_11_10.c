92a19838a1f9fb746b61510076dd8f9b629f9d5c
panguangwei <2712278005@qq.com>
2022-11-22 18:22:04 +0800

    操作系统作业

--- 算法练习/test_11_10.c
+++ 算法练习/test_11_10.c
@@ -12,42 +12,42 @@ int maxres[5], running[5], safe = 0;    //Safe是安全状态
 int count = 0, i, j, exec, r, p, k = 1; //Exec是执行的进程数
 
 int main() {
-    printf("\nEnter the number of processes: ");    //输入进程数
+    printf("\n请输入进程数: ");    //输入进程数
     scanf("%d", &p);    //p是进程数
     for (i = 0; i < p; i++) {
         running[i] = 1;
         count++;
     }
-    printf("\nEnter the number of resources: ");    //输入资源数
+    printf("\n请输入资源数: ");    //输入资源数
     scanf("%d", &r);    //r是资源数
-    printf("\nEnter Claim Vector:");
+    printf("\n请输入索赔向量:");
     for (i = 0; i < r; i++) {
         scanf("%d", &maxres[i]);
     }
-    printf("\nEnter Allocated Resource Table:\n");  //输入已分配的资源表
+    printf("\n请输入已分配的资源表:\n");  //输入已分配的资源表
     for (i = 0; i < p; i++) {
         for (j = 0; j < r; j++) {
             scanf("%d", &curr[i][j]);   //输入当前进程已分配的资源
         }
     }
-    printf("\nEnter Maximum Claim Table:\n");   //输入最大需求表
+    printf("\n请输入最大需求表:\n");   //输入最大需求表
     for (i = 0; i < p; i++) {
         for (j = 0; j < r; j++) {
             scanf("%d", &maxclaim[i][j]);   //输入当前进程最大需求的资源
         }
     }
-    printf("\nThe Claim Vector is: ");  //输出资源数
+    printf("\n输出资源数: ");  //输出资源数
     for (i = 0; i < r; i++) {
         printf("\t%d", maxres[i]);  //输出最大资源数
     }
-    printf("\nThe Allocated Resource Table:\n");    //输出已分配的资源表
+    printf("\n输出已分配的资源表:\n");    //输出已分配的资源表
     for (i = 0; i < p; i++) {
         for (j = 0; j < r; j++) {
             printf("\t%d", curr[i][j]); //输出当前进程已分配的资源
         }
         printf("\n");
     }
-    printf("\nThe Maximum Claim Table:\n"); //输出最大需求表
+    printf("\n输出最在需求表:\n"); //输出最大需求表
     for (i = 0; i < p; i++) {
         for (j = 0; j < r; j++) {
             printf("\t%d", maxclaim[i][j]); //输出当前进程最大需求的资源
@@ -59,14 +59,14 @@ int main() {
             alloc[j] += curr[i][j]; //计算已分配的资源
         }
     }
-    printf("\nAllocated resources:");   //输出已分配的资源
+    printf("\n输出已分配的资源:");   //输出已分配的资源
     for (i = 0; i < r; i++) {
         printf("\t%d", alloc[i]);   //输出已分配的资源
     }
     for (i = 0; i < r; i++) {
         avl[i] = maxres[i] - alloc[i];  //计算可用资源
     }
-    printf("\nAvailable resources:");   //输出可用资源
+    printf("\n输出可用资源:");   //输出可用资源
     for (i = 0; i < r; i++) {
         printf("\t%d", avl[i]); //输出可用资源
     }
@@ -97,11 +97,11 @@ int main() {
             }
         }
         if (!safe) {    //如果不安全
-            printf("\nThe processes are in unsafe state.\n");
+            printf("\n这个线程处于不安全的状态.\n");
             break;
         } else {    //如果安全
-            printf("\nThe process is in safe state");
-            printf("\nAvailable vector:");
+            printf("\n这个线程处于安全的状态.");
+            printf("\n可用的资源:");
             for (i = 0; i < r; i++) {   //输出可用资源
                 printf("\t%d", avl[i]);
             }

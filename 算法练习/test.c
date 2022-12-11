522ffe1b9558c4126ff2166cff80dc8a5feaf056
panguangwei <2712278005@qq.com>
2022-12-11 23:47:29 +0800

    12-11

---
+++ 算法练习/test.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+#define N 10
+
+int main() {
+    int s;
+    int a[N];
+    int sum = 0;
+    for (int s = 0; s < 10; ++s) {
+        scanf("%d", &a[s]);
+        if (a[s] % 2 == 0) {
+            sum++;
+        }
+    }
+    printf("sum=%d", sum);
+    return 0;
+}
